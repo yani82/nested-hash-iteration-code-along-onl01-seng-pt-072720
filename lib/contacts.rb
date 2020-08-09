fdf1907ed23cd4ae302fbfbe87e40cf9c38fbc87
Yani Asari <yani.iman1@gmail.com>
2020-08-09 04:23:38 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,5 +16,12 @@ require 'pry'
   
 def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
-  binding.pry
+  if person == "Freddy Mercury"
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_icecream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+              end
+          end
+      end
+    end
 end 
