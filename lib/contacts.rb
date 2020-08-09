fd5325f64db7addd9e3edf4bdb44a1f817fd5afa
Yani Asari <yani.iman1@gmail.com>
2020-08-09 04:06:35 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,18 +15,6 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
- contacts = {
-  "Jon Snow" => {
-    name: "Jon",
-    email: "jon_snow@thewall.we",
-    favorite_ice_cream_flavors: ["chocolate", "vanilla"]
-  },
-  "Freddy Mercury" => {
-    name: "Freddy",
-    email: "freddy@mercury.com",
-    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-  }
-}
 freddy = contacts["Freddy Mercury"]
   freddy.each do |attribute, value|
     if attribute == :favorite_icecream_flavors
